3843e063ce431beb04e511e2fd3713a2d247d6cc
TONMOY-CSE <tonmoydey2025@gmail.com>
2025-02-16 01:24:55 +0600

    Add files via upload

---
+++ class managment systeam for B7 final.cpp
@@ -0,0 +1,315 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+struct student{
+    string name;
+    int AGE;
+    string id;
+
+
+void setinfo(){
+    cout<<setw(70)<<"WELLCOME TO OUR CLASS MANGMENT SYSTEAM\n";
+    cout<<setw(70)<<"......................................\n";
+
+
+
+
+
+    cout<< setw(70) <<" ONLY FOR B7 \n";
+    cout<< setw(70) <<"..............\n";
+
+    cout<<"Enter your name:\n";
+    cout<<".................\n";
+    cin>>name;
+
+
+
+}
+
+void display(){
+    if(name=="Tonmoy"){
+            cout<<"id:24-59325-3\n";
+            cout<<"AGE:20\n";
+            cout<<"GROUP:B7\n";
+    }
+    else if(name=="SALEH"){
+        cout<<"id :24-59323-3\n";
+        cout<<"AGE:21\n";
+        cout<<"GROUP:B7\n";
+    }
+
+    else if(name=="ARPON"){
+        cout<<"id:24-59326-3\n";
+        cout<<"AGE:22\n";
+        cout<<"GROUP:B7\n";
+    }
+    else if(name=="kamrul"){
+        cout<<"id:24-59327-3\n";
+        cout<<"AGE:23\n";
+        cout<<"GROUP:B7\n";
+    }
+
+    else if(name=="sujoy"){
+        cout<<"id:24-59329-3\n";
+        cout<<"AGE:24\n";
+         cout<<"GROUP:B7\n";
+    }
+    else if(name=="Tuhin"){
+        cout<<"id:24-59330-3\n";
+        cout<<"AGE:25\n";
+        cout<<"GROUP:B7\n";
+
+    }
+
+    else if (name=="saha padmasree"){
+        cout<<"id:24-59331-3\n";
+        cout<<"AGE:20\n";
+        cout<<"GROUP:B7\n";
+    }
+    else if(name=="siam"){
+        cout<<"id:24-59336-3\n";
+        cout<<"AGE:21\n";
+        cout<<"GROUP:B7\n";
+
+    }
+
+    else if(name=="akash ali"){
+        cout<<"id:24-59337-3\n";
+        cout<<"AGE:22\n";
+        cout<<"GROUP:B7\n";
+
+    }
+
+    else if(name=="sabbir"){
+        cout<<"id:24-59338-3\n";
+        cout<<"AGE:23\n";
+        cout<<"GROUP:B7\n";
+
+    }
+
+    else if(name=="mahi"){
+        cout<<"id:24-59340-3\n";
+        cout<<"AGE:24\n";
+        cout<<"GROUP:B7\n";
+    }
+    else if(name=="islam"){
+        cout<<"id:24-59342-3";
+        cout<<"AGE:25";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="Emon"){
+        cout<<"id:24-59344-3";
+        cout<<"AGE:20";
+        cout<<"GROUP:B7";
+
+    }
+    else if(name=="kamal"){
+        cout<<"id:24-59345-3";
+        cout<<"AGE:21";
+        cout<<"GROUP:B7";
+    }
+
+    else if(name=="raiyan"){
+        cout<<"id:24-59970-3";
+        cout<<"AGE:22";
+        cout<<"GROUP:B7";
+
+    }
+
+    else if(name=="fahim"){
+        cout<<"id:24-60352-3";
+        cout<<"AGE:23";
+        cout<<"GROUP:B7";
+
+    }
+
+    else if(name=="fabiha"){
+        cout<<"id:24-60482-3";
+        cout<<"AGE:24";
+        cout<<"GROUP:B7";
+
+    }
+    else if(name=="latif"){
+        cout<<"id:24-59279-3";
+        cout<<"AGE:20";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="Mahrij"){
+        cout<<"id:24-59281-3";
+        cout<<"AGE:21";
+        cout<<"GROUP:B7";
+
+    }
+
+    else if(name=="shafiqul"){
+
+        cout<<"id:24-59282-3";
+        cout<<"AGE:22";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="fardin"){
+        cout<<"id:24-59284-3";
+        cout<<"AGE:23";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="saif al"){
+        cout<<"id:24-59285-3";
+        cout<<"AGE:24";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="shaiin"){
+        cout<<"id:24-59286-3";
+        cout<<"AGE:25";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="yeasin"){
+        cout<<"id:24-59287-3";
+        cout<<"AGE:20";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="MEDHA"){
+        cout<<"id:24-59288-3";
+        cout<<"AGE:21";
+        cout<<"GROUP:B7";
+
+    }
+    else if(name=="roy"){
+        cout<<"id:24-59289-3";
+        cout<<"AGE:22";
+        cout<<"GROUP:B7";
+
+    }
+    else if(name=="abid"){
+        cout<<"id:24-59291-3";
+        cout<<"AGE:23";
+        cout<<"GROUP:B7";
+
+    }
+    else if(name=="bhakta"){
+        cout<<"id:24-59292-3";
+        cout<<"AGE:24";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="tonim"){
+        cout<<"id:24-59293-3";
+        cout<<"AGE:25";
+        cout<<"GROUP:B7";
+
+    }
+    else if(name=="AL"){
+        cout<<"id:24-59294-3";
+        cout<<"AGE:20";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="asif"){
+        cout<<"id:24-59299-3";
+        cout<<"AGE:21";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="sakib"){
+        cout<<"id:24-59301-3";
+        cout<<"AGE:22";
+        cout<<"GROUP:B7";
+
+    }
+    else if(name=="NOWRIN"){
+        cout<<"id:24-59307-3";
+        cout<<"AGE:23";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="mehajabin"){
+        cout<<"id:24-59310-3";
+        cout<<"AGE:24";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="shaown"){
+        cout<<"id:24-59312-3";
+        cout<<"AGE:25";
+        cout<<"GROUP:B7";
+    }
+    else if(name=="md shaown"){
+        cout<<"id:24-59313-3";
+        cout<<"AGE:20";
+        cout<<"GROUP:B7";
+
+    }
+    else if(name=="hasnat"){
+        cout<<"id:24-59315-3";
+        cout<<"AGE:21";
+        cout<<"GROUP:B7";
+
+
+    }
+
+    else if(name=="sabbir"){
+        cout<<"id:24-59317-3";
+        cout<<"AGE:22";
+        cout<<"GROUP:B7";
+    }
+
+    else if(name=="chowa"){
+        cout<<"id:24-59319-3";
+        cout<<"AGE:23";
+        cout<<"GROUP:B7";
+
+
+
+    }
+    else if(name=="litu"){
+        cout<<"id:24-59322-3";
+        cout<<"AGE:24";
+        cout<<"GROUP:B7";
+    }
+
+
+    else{
+        cout<<"student not found!";
+    }
+}
+};
+
+
+int main(){
+
+    student s;
+    s.setinfo();
+    s.display();
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
